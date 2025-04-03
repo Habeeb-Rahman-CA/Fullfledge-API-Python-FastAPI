d82c2bf311b0ab501c65435e5bc036f8a590044d
Habeeb-Rahman-CA <habeebrahmanca07@gmail.com>
2025-04-03 22:35:44 +0530

    Added: HTTP Request

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
 @app.get('/')
 async def root():
     return {"message": "Hello World"}
+
+@app.get('/posts')
+async def get_posts():
+    return {"data": "This is your posts"}
+
+@app.post('/posts')
+async def create_post(payload: dict = Body(...)):
+    print(payload)
+    return {"message": "Successfully created post!"}
