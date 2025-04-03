2068888ee041ba3d01ef40edf83cb8c60f316e15
Habeeb-Rahman-CA <habeebrahmanca07@gmail.com>
2025-04-03 22:46:50 +0530

    Added: BaseModel for Post

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get('/')
 async def root():
@@ -12,6 +20,6 @@ async def get_posts():
     return {"data": "This is your posts"}
 
 @app.post('/posts')
-async def create_post(payload: dict = Body(...)):
-    print(payload)
-    return {"message": "Successfully created post!"}
+async def create_post(new_post: Post):
+    print(new_post.model_dump())
+    return {"message": "New post uploaded!"}
