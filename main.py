74559fd2afa7889807140cabfbde5933220585de
Habeeb-Rahman-CA <habeebrahmanca07@gmail.com>
2025-04-05 00:48:03 +0530

    Generated Id randomly

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
 
 app = FastAPI()
 
@@ -11,15 +12,30 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
+my_posts = [
+    {
+        "id": 1,
+        "title": "Title of post 1",
+        "content": "Content of post 1"
+    },
+    {
+        "id": 2,
+        "title": "Title of post 2",
+        "content": "Content of post 2"
+    },
+    ]
+
 @app.get('/')
 async def root():
     return {"message": "Hello World"}
 
 @app.get('/posts')
 async def get_posts():
-    return {"data": "This is your posts"}
+    return {"data": my_posts}
 
 @app.post('/posts')
-async def create_post(new_post: Post):
-    print(new_post.model_dump())
-    return {"message": "New post uploaded!"}
+async def create_post(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 100)
+    my_posts.append(post_dict)
+    return {"message": "New post uploaded!", "data": post_dict}
