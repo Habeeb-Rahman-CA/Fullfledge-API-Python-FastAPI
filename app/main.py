822daaf89951d53e0bd39ccad336a10a9353ab15
Habeeb-Rahman-CA <habeebrahmanca07@gmail.com>
2025-04-06 00:26:52 +0530

    Changed CRUD using PSQL statement without using ORMs

--- app/main.py
+++ app/main.py
@@ -55,43 +55,48 @@ def find_index_post(id):
 async def root():
     return {"message": "Hello World"}
 
+# Get All Post
 @app.get('/posts')
 async def get_posts():
-    return {"data": my_posts}
+    cursor.execute("""SELECT * FROM posts""")
+    posts = cursor.fetchall()
+    return {"message":"Retrieving all posts."," data": posts}
 
-@app.post('/posts', status_code=status.HTTP_201_CREATED)
-async def create_post(post: Post):
-    post_dict = post.dict()
-    post_dict['id'] = randrange(0, 100)
-    my_posts.append(post_dict)
-    return {"message": "New post uploaded!", "data": post_dict}
-
-@app.get('/posts/latest')
-async def get_latest_post():
-    post = my_posts[len(my_posts)-1]
-    return {"message": "Loaded latest post", "detail": post}
-
+# Get Post By Id
 @app.get('/posts/{id}')
-async def get_post(id: int):
-    post = find_post(id)
+async def get_post(id: str):
+    cursor.execute("""SELECT * FROM posts WHERE id = %s""", (id))
+    post = cursor.fetchone()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id:{id} not found!")
     return {"message": f"Here is post {id}!", "data": post}
 
+# Create Post
+@app.post('/posts', status_code=status.HTTP_201_CREATED)
+async def create_post(post: Post):
+    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """,
+                   (post.title, post.content, post.published))
+    new_post = cursor.fetchone()
+    conn.commit()
+    return {"message": "New post uploaded!", "data": new_post}
+
+# Delete Post By Id
 @app.delete('/posts/{id}', status_code=status.HTTP_204_NO_CONTENT)
-async def delete_post(id:int):
-    index = find_index_post(id)
-    if index == None:
+async def delete_post(id:str):
+    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (id))
+    deleted_post = cursor.fetchone()
+    conn.commit()
+    if deleted_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id:{id} doesn't exist!")
-    my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
+# Update Post By Id
 @app.put('/posts/{id}')
-async def update_post(id: int, post: Post):
-    index = find_index_post(id)
-    if index == None:
+async def update_post(id: str, post: Post):
+    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",
+                   (post.title, post.content, post.published, id))
+    updated_post = cursor.fetchone()
+    conn.commit()
+    if updated_post == None:
          raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id:{id} doesn't exist!")
-    post_dict = post.dict()
-    post_dict['id'] = id
-    my_posts[index] = post_dict
-    return {"message": f"Post {id} updated!", "data": post_dict}
+    return {"message": f"Post {id} updated!", "data": updated_post}
