347492196237153934156b120db9e4f698a9b3b6
Habeeb-Rahman-CA <habeebrahmanca07@gmail.com>
2025-04-06 12:09:43 +0530

    change get_db to database and import in main

--- app/main.py
+++ app/main.py
@@ -6,19 +6,13 @@ from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
+from sqlalchemy.orm import Session
 from . import models
-from .database import engine, SessionLocal
+from .database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
 class Post(BaseModel):
     title: str
